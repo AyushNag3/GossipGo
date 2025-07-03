715eeefa6bdebf86012863aa5b6ccad318eb801d
AyushNag3 <mnag7278@gmail.com>
2025-07-03 16:44:14 +0530

    changes in cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: "*",
+    origin: "https://gossip-go-ayush-nags-projects.vercel.app",
     credentials: true,
   }),
 )
